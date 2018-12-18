762120d3b44efec32a8ecd14579a4595eb1a3fa8
akinmaurice <akinmaurice@gmail.com>
2018-12-18 18:48:46 +0100

    add navigation links to header

--- client/src/components/Views/Header.js
+++ client/src/components/Views/Header.js
@@ -9,7 +9,13 @@ const Header = () => {
         <Link to="/" className="navbar-brand py-0">Student-App </Link>
         <ul className="navbar-nav">
           <li className="nav-item">
-            <Link to="/" className="nav-link__color">Go Home</Link>
+            <Link to="/" className="nav-link__color">Home</Link> &nbsp;
+          </li>
+          <li className="nav-item">
+            <Link to="/students" className="nav-link__color">students</Link> &nbsp;
+          </li>
+          <li className="nav-item">
+            <Link to="/create" className="nav-link__color">new</Link> &nbsp;
           </li>
         </ul>
       </div>
